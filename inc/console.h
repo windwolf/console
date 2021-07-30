053f65446ac0396a53bd9bd10212715f1267f120
windwolf <zhoujian.ww@gmail.com>
2021-07-30 15:20:42 +0800

    console的init commit

---
+++ inc/console.h
@@ -0,0 +1,45 @@
+#ifndef ___CONSOLE_H__
+#define ___CONSOLE_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "stdint.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "shared.h"
+#include "../../communication_protocol/inc/tree_accessor.h"
+#include "../../communication_protocol/inc/message_parser.h"
+#include "../../drivers/common/inc/common/stream.h"
+
+#define CONSOLE_CMD_TEXT_BUFFER_MAX_SIZE 1024
+
+#define CONSOLE_ERROR_TEXT_COMMAND_PARSE "command parse error."
+#define CONSOLE_ERROR_TEXT_COMMAND_NOT_RECOGNIZE "command can not be recognized."
+#define CONSOLE_ERROR_TEXT_PATH_ARGUMENT_NEEDED "path argument needed."
+#define CONSOLE_ERROR_TEXT_VALUE_ARGUMENT_NEEDED "value argument needed."
+#define CONSOLE_ERROR_TEXT_CHANGE_CONTEXT_FAILED "change context failed."
+#define CONSOLE_ERROR_TEXT_VALUE_DISPLAY_ERROR "value display error."
+#define CONSOLE_ERROR_TEXT_VALUE_GET_FAILED "value get failed."
+#define CONSOLE_ERROR_TEXT_VALUE_SET_FAILED "value set failed."
+
+    typedef struct Console
+    {
+        Stream *dev;
+        TreeAccessor *tree;
+        MessageParser *parser;
+        MessageFrame frame;
+        char cmdTextBuffer[CONSOLE_CMD_TEXT_BUFFER_MAX_SIZE];
+    } Console;
+
+    void console_init(Console *console);
+
+    void console_start(Console *console);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ___CONSOLE_H__
